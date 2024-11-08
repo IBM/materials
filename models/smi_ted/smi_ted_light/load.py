Fix SMILES normalization error
Signed-off-by: vichShir <victor.shirasuna@gmail.com>
Signed-off-by: Lisa Hamada <Lisa.Hamada@ibm.com>

models/smi_ted/smi_ted_light/load.py
@@ -19,6 +19,7 @@ from huggingface_hub import hf_hub_download
 # Data
 import numpy as np
 import pandas as pd
+import numpy as np
 
 # Chemistry
 from rdkit import Chem
@@ -497,7 +498,11 @@ class Smi_ted(nn.Module):
 
         # handle single str or a list of str
         smiles = pd.Series(smiles) if isinstance(smiles, str) else pd.Series(list(smiles))
+
+        # SMILES normalization
         smiles = smiles.apply(normalize_smiles)
+        null_idx = smiles[smiles.isnull()].index.to_list()  # keep track of SMILES that cannot normalize
+        smiles = smiles.dropna()
         
         # tokenizer
         idx, mask = self.tokenize(smiles.to_list())
@@ -536,6 +541,30 @@ class Smi_ted(nn.Module):
         # reconstruct tokens
         pred_ids = self.decoder.lang_model(pred_cte.view(-1, self.max_len, self.n_embd))
         pred_ids = torch.argmax(pred_ids, axis=-1)
+
+        # replacing null SMILES with NaN values
+        for idx in null_idx:
+            true_ids = true_ids.tolist()
+            pred_ids = pred_ids.tolist()
+            true_cte = true_cte.tolist()
+            pred_cte = pred_cte.tolist()
+            true_set = true_set.tolist()
+            pred_set = pred_set.tolist()
+
+            true_ids.insert(idx, np.array([np.nan]*self.config['max_len']))
+            pred_ids.insert(idx, np.array([np.nan]*self.config['max_len']))
+            true_cte.insert(idx, np.array([np.nan] * (self.config['max_len']*self.config['n_embd'])))
+            pred_cte.insert(idx, np.array([np.nan] * (self.config['max_len']*self.config['n_embd'])))
+            true_set.insert(idx, np.array([np.nan]*self.config['n_embd']))
+            pred_set.insert(idx, np.array([np.nan]*self.config['n_embd']))
+        
+        if len(null_idx) > 0:
+            true_ids = torch.tensor(true_ids)
+            pred_ids = torch.tensor(pred_ids)
+            true_cte = torch.tensor(true_cte)
+            pred_cte = torch.tensor(pred_cte)
+            true_set = torch.tensor(true_set)
+            pred_set = torch.tensor(pred_set)
         
         return ((true_ids, pred_ids), # tokens
                (true_cte, pred_cte),  # token embeddings
@@ -569,10 +598,14 @@ class Smi_ted(nn.Module):
 
         # handle single str or a list of str
         smiles = pd.Series(smiles) if isinstance(smiles, str) else pd.Series(list(smiles))
+
+        # SMILES normalization
         smiles = smiles.apply(normalize_smiles)
-        n_split = smiles.shape[0] // batch_size if smiles.shape[0] >= batch_size else smiles.shape[0]
-        
+        null_idx = smiles[smiles.isnull()].index.to_list()  # keep track of SMILES that cannot normalize
+        smiles = smiles.dropna()
+
         # process in batches
+        n_split = smiles.shape[0] // batch_size if smiles.shape[0] >= batch_size else smiles.shape[0]
         embeddings = [
             self.extract_embeddings(list(batch))[2].cpu().detach().numpy() 
                 for batch in tqdm(np.array_split(smiles, n_split))
@@ -584,8 +617,13 @@ class Smi_ted(nn.Module):
             torch.cuda.empty_cache()
             gc.collect()
 
+        # replacing null SMILES with NaN values
+        for idx in null_idx:
+            flat_list.insert(idx, np.array([np.nan]*self.config['n_embd']))
+        flat_list = np.asarray(flat_list)
+
         if return_torch:
-            return torch.tensor(np.array(flat_list))
+            return torch.tensor(flat_list)
         return pd.DataFrame(flat_list)
     
     def decode(self, smiles_embeddings):